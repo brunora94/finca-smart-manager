47355845feed9e2053d6ed66a889f679caa0ac29
bruno <brunora94@gmail.com>
2026-01-07 10:12:05 +0100

    Fix: Use correct 'datasources' config object for PrismaClient to fix build type error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const prismaClientSingleton = () => {
     try {
         return new PrismaClient({
-            datasourceUrl: process.env.DATABASE_URL
+            datasources: {
+                db: {
+                    url: process.env.DATABASE_URL,
+                },
+            },
         })
     } catch (e) {
         console.error("Failed to initialize Prisma Client:", e);
