3f9cc337f2f00344b8d100452e15944a65b1940a
bruno <brunora94@gmail.com>
2026-01-07 08:45:40 +0100

    Fix seed script (retry) - remove sqlite adapter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,6 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
-const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL!.replace('file:', '')
-})
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient()
 
 async function main() {
     console.log('Seeding database...')
